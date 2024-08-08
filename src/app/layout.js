17aa0a412709011bdc48dc63f41dc1e1885fc6bb
rupokghosh <rupokghosh291@gmail.com>
2024-08-08 11:09:20 -0700

    add metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,35 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import image from "../../public/image.png";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "BuildIt",
-  description: "Build your habits and shape your life",
+  description:
+    "Application that helps you build your habits and shape your life",
+  keywords: "habit tracker, habits, minimal habit tracker",
+  openGraph: {
+    title: "BuildIt",
+    description:
+      "Application that helps you build your habits and shape your life",
+    url: "https://buildit-habitracker.netlify.app/",
+    images: [
+      {
+        url: { image },
+        width: 1200,
+        height: 630,
+        alt: "BuildIt - Habit Tracker",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "BuildIt",
+    description:
+      "Application that helps you build your habits and shape your life",
+    images: [{ image }],
+  },
 };
 
 export default function RootLayout({ children }) {
